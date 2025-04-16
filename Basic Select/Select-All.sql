f58c8413f5f95dcb4387404d1f10e89e41d84b59
1thallo <ithalloflu@gmail.com>
2025-04-15 23:53:00 -0300

    add: Resolução do exercício "Select All"

---
+++ Basic Select/Select-All.sql
@@ -0,0 +1,22 @@
+/*
+  Desafio HackerRank: Select All  
+  Link: https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+
+  Enunciado:
+  --------------------------------------------------
+  Escreva uma consulta SQL que retorne *todas* as colunas e *todas* as linhas 
+  da tabela CITY.
+
+  Estrutura da Tabela: CITY
+  --------------------------------------------------
+  | Coluna       | Tipo      | Descrição                          |
+  |--------------|-----------|------------------------------------|
+  | ID           | INT       | Identificador único da cidade      |
+  | NAME         | VARCHAR   | Nome da cidade                     |
+  | COUNTRYCODE  | VARCHAR   | Código do país (ex: 'USA')         |
+  | DISTRICT     | VARCHAR   | Estado ou distrito da cidade       |
+  | POPULATION   | INT       | População da cidade                |
+
+  Minha Solução:
+*/
+SELECT * FROM CITY;
